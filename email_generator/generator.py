24d986b79dca84e79c490cab70f0fbe8fd11776f
unknown <angelodepadua2@gmail.com>
2025-06-30 23:35:48 +0800

    feat: integrate Faker lib for dynamic generation

--- email_generator/generator.py
+++ email_generator/generator.py
@@ -1,3 +1,6 @@
+import random
+from faker import Faker
+from datetime import datetime, timedelta
 from .templates import ham_senders, spam_senders, ham_templates, spam_templates, variables
 
 class EmailDatasetGenerator:
@@ -7,3 +10,15 @@ class EmailDatasetGenerator:
         self.ham_templates = ham_templates
         self.spam_templates = spam_templates
         self.variables = variables
+        self.faker = Faker()
+    
+    def generate_ham_email(self):
+        category = random.choice(list(self.ham_senders.keys()))
+        sender = random.choice(self.ham_senders[category])
+
+        template_category = random.choice(list(self.ham_templates.keys()))
+        subject_template, content_template = random.choice(self.ham_templates[template_category])
+
+        template_vars = {
+ 
+        }
