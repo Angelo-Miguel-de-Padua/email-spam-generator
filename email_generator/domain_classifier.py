a68e870ef116c3198873d95d2f69463f016589b0
unknown <angelodepadua2@gmail.com>
2025-07-02 03:32:10 +0800

    feat: normalize lowercase text and make a variable to store scores

--- email_generator/domain_classifier.py
+++ email_generator/domain_classifier.py
@@ -3,5 +3,5 @@ from email_generator.utils.category_keywords import CATEGORY_KEYWORDS
 min_keyword_matches = 2
 
 def classify_text(text: str) -> tuple[str, dict]:
- 
- 
+    text = text.lower
+    scores = {}
