5f0b0ae8865efd672c9a8da4058fd627a701c826
unknown <angelodepadua2@gmail.com>
2025-07-02 04:41:01 +0800

    feat: open csv file

--- email_generator/classifier/load_tranco.py
+++ email_generator/classifier/load_tranco.py
@@ -2,4 +2,5 @@ import csv
 
 def load_tranco_domains(path: str = "resources/top-1m.csv", limit: int = 5000) -> list[str]:
     domains = []
-    
+
+    with open(path, newline="", encoding="utf-8") as csvfile:
