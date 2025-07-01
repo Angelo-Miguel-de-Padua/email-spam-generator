59102f2c66eb254f368c0ff55857b7b87b44fd5b
unknown <angelodepadua2@gmail.com>
2025-07-02 05:10:45 +0800

    feat: added more user agents and randomize it for stealth

--- email_generator/classifier/scraper.py
+++ email_generator/classifier/scraper.py
@@ -1,13 +1,24 @@
 import requests
+import random
 from bs4 import BeautifulSoup
 from email_generator.classifier.classifier import classify_text
 from email_generator.classifier.text_extractor import extract_text
+
+def random_user_agent() -> str:
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
+    ]
+    return random.choice(user_agents)
 
 def scraper(domain: str) -> dict:
     url = f"http://{domain}"
 
     try:
-        response = requests.get(url, timeout=5, headers={"User-Agent": "Mozilla/5.0"})
+        response = requests.get(url, timeout=5, headers={"User-Agent": random_user_agent()})
         soup = BeautifulSoup(response.text, "html.parser")
 
         base_text = extract_text(soup, max_paragraphs=1)
