2a6d46a0cf039a3d4e859f60b560912452eeee0d
unknown <angelodepadua2@gmail.com>
2025-07-02 04:41:30 +0800

    feat: parse file as CSV using sv.reader

--- email_generator/classifier/load_tranco.py
+++ email_generator/classifier/load_tranco.py
@@ -4,3 +4,4 @@ def load_tranco_domains(path: str = "resources/top-1m.csv", limit: int = 5000) -
     domains = []
 
     with open(path, newline="", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
