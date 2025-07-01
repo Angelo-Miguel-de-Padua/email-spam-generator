6cc316bcc7cfcfaf697be3c36a1f5396d389c249
unknown <angelodepadua2@gmail.com>
2025-07-02 04:26:31 +0800

    feat: fetch domain HTML and parse it using BeautifulSoup

--- email_generator/classifier/scraper.py
+++ email_generator/classifier/scraper.py
@@ -1,4 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 def scraper(domain: str) -> dict:
     url = f"http://{domain}"
+
+    try:
+        response = requests.get(url, timeout=5, headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(response.text, "html.parser")
