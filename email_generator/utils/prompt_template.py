58e32a6e9725d84cec2525d5e94133c853a57a89
unknown <angelodepadua2@gmail.com>
2025-07-02 19:29:12 +0800

    feat: make the prompt for mixtral

--- email_generator/utils/prompt_template.py
+++ email_generator/utils/prompt_template.py
@@ -3,3 +3,21 @@ CATEGORY_LIST = [
     "media", "social", "forum", "health", "real_estate", "gaming", "sports",
     "adult", "cloud", "ai", "crypto", "security", "government", "general"
 ]
+
+def build_prompt(text: str) -> str:
+    categories = ", ".join(CATEGORY_LIST)
+    return f"""
+You are an expert in web content classifier.
+
+Your task is to read the following website content and categorize it into one of the following categories:
+{categories}
+
+### WEBSITE TEXT:
+{text}
+
+### INSTRUCTION:
+Respond with only the most appropriate category name from the list above.
+Do not explain.
+Do not add extra punctuation or words.
+Just reply with the category.
+"""
