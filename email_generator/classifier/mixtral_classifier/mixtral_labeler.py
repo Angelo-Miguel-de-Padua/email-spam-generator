4f6e0c911cdb319081af408966e662ce1261f4d3
unknown <angelodepadua2@gmail.com>
2025-07-03 02:43:40 +0800

    feat: define model name and scraped_file/labeled_file location

--- email_generator/classifier/mixtral_classifier/mixtral_labeler.py
+++ email_generator/classifier/mixtral_classifier/mixtral_labeler.py
@@ -1,5 +1,13 @@
+import os
 import json
+import requests
+from dotenv import load_dotenv
 from email_generator.classifier.mixtral_classifier.mixtral_scraper import scrape_and_extract
 from email_generator.utils.prompt_template import build_prompt
-import requests
+from email_generator.utils.domain_utils import normalize_domain
 
+load_dotenv()
+
+OLLAMA_MODEL_NAME = "mixtral:8x7b-instruct-v0.1-q4_K_M"
+scraped_file = "../resources/scraped_data.json"
+labeled_file = "../resources/labeled_data.json"
