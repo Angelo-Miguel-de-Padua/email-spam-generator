e7cbd794a871af629b20e5b80309d3c35e6aef04
unknown <angelodepadua2@gmail.com>
2025-07-04 22:32:55 +0800

    feat: add a util that checks if the domain name is valid

--- email_generator/utils/domain_utils.py
+++ email_generator/utils/domain_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 def load_tranco_domains(csv_path, limit=500):
     df = pd.read_csv(csv_path, header=None, names=["rank", "domain"])
@@ -9,3 +10,10 @@ def normalize_domain(domain: str) -> str:
     if domain.startswith("www."):
         domain = domain[4:]
     return domain
+
+def is_valid_domain(domain: str) -> bool:
+    if len(domain) > 253:
+        return False
+    
+    pattern = r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.(?!-)[A-Za-z0-9-]{1,63}(?<!-))*$"
+    return re.fullmatch(pattern, domain) is not None
