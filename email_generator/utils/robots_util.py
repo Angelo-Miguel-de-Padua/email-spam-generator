0d469ddd10d391ca207ab2da01e40ef80544c94d
unknown <angelodepadua2@gmail.com>
2025-07-05 16:35:34 +0800

    chore: delete cache entries older than 1 day

--- email_generator/utils/robots_util.py
+++ email_generator/utils/robots_util.py
@@ -1,7 +1,9 @@
 import os
 import json
+import time
 
 ROBOTS_CACHE_FILE = "resources/robots_cache.json"
+CACHE_TTL_SECONDS = 86400 # 1day
 
 _robots_cache = {}
 _cache_loaded = False
@@ -15,3 +17,10 @@ def _load_robots_cache():
         except json.JSONDecodeError:
             pass
     _cache_loaded = True
+
+def _cleanup_expired_entries():
+    now = int(time.time())
+    expired = [d for d, e in _robots_cache.items()
+                if now - e.get("fetched_at", 0) > CACHE_TTL_SECONDS]
+    for d in expired:
+        del _robots_cache[d]
